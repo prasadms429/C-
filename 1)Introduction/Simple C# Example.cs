9b1289f092e551074ef56fe868d7250d529cf879
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-05-18 10:50:06 +0530

    Create Simple C# Example.cs

---
+++ 1)Introduction/Simple C# Example.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This will add phrase "Hello world!" to the console.
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();          // If we dont write this line then console window appear for some time and it automatically goes off. By this Readkey(), the console will not goes off until we press any key on keyboard.
+        }
+    }
+}
