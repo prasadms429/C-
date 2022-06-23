7a82283c9c3b9c91d2bcc75be06fc76f62548d26
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-06-23 18:54:00 +0530

    Create Strings.cs

---
+++ 4)DataType/Strings.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is \"Prasad M S\"");   // It will print "Prasad M S" with cotated " "
+            Console.WriteLine("The new path is C:\\Users\\directory");    // To print single "\", we use this "\\"
+            Console.WriteLine("Typing backlash n in a string\n will cause a line break");
+
+            Console.ReadLine();
+        }
+    }
+}
