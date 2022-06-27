230652a8d857af1d54af75a3eb7bcf141b6ff1da
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-06-27 23:39:51 +0530

    Update Car.cs

--- 5) Object-oriented  programming/2) Constructors/Car.cs
+++ 5) Object-oriented  programming/2) Constructors/Car.cs
@@ -1,1 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConstructorLecture
+{
+    internal class Car
+    {
+        public string color;
+        public string brand;
+        public int currentSpeed;
+
+        public Car()        // non-parametraised constructor
+        {
+            currentSpeed = 0;   // Constructor initializes values when an object is created and we dont need to intialize it again and again.
+        }
+
+        public Car(string carColor,string carBrand) // paramatrised constructor
+        {
+            color = carColor;
+            brand = carBrand;
+            currentSpeed = 0;
+        }
+
+        public void Accelerate(int increaseSpeed)
+        {
+            currentSpeed += increaseSpeed;
+        }
+
+        public void Deccelerate(int decreaseSpeed)
+        {
+            currentSpeed -= decreaseSpeed;
+        }
+
+        public void DisplaySpeed()
+        {
+            Console.WriteLine($"The {color} {brand} is travelling at {currentSpeed} km/h");
+        }
+    }
+}
