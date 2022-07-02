df00597c8779214383aa99087fc30feebf8fd61a
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-07-02 10:23:46 +0530

    Update and rename RaceCar.cs to 2)RaceCar.cs

--- 5) Object-oriented  programming/Inheritance/2)RaceCar.cs
+++ 5) Object-oriented  programming/Inheritance/2)RaceCar.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace Inheritance
 {
-    internal class RaceCar : Car
+    internal class RaceCar : Car        // To inherit from base class we use like this " : Car"
     {
         private int numNitros = 5;
 
-        public RaceCar(int numNitros) : base()
+public RaceCar(int numNitros) : base() // when we call derived class constructor, if we want to call base class constructor and initalize it, then we use this syntax " : base()"
         {
             this.numNitros = numNitros;
         }
@@ -20,7 +20,7 @@ namespace Inheritance
             this.numNitros=numNitros;
         }
 
-        public override void DisplayInfo()
+        public override void DisplayInfo()  // override method is used add some extra functionality to base method and use it in derived method.
         {
             base.DisplayInfo();
             Console.WriteLine($"The {color} {brand} is travelling at {currentSpeed} km/h and has {numNitros} nitros");
