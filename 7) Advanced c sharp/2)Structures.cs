e98a1b685a3a43be904e1ec2190d61d817cf1065
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-07-03 08:12:33 +0530

    Create 2)Structures.cs

---
+++ 7) Advanced c sharp/2)Structures.cs
@@ -0,0 +1,9 @@
+// Structures is almost same as class. It is defined using keyword struct such as " struct vector " .
+/* The structures are value type whereas classes are reference type that stores address.
+ex:- vector Myvector= new vector(1,2,3);
+vector Myvector2;
+Myvector2= Myvector // Makes a copy of each field.
+*/
+// Strutures are cannot be used for inheritance.
+// we cannot assign values to fields in strucutes.
+// Structures is well suited for less amount of data (if we want to use few fields such as 5 or less then it is well suited ) and must not contain inheritance.
